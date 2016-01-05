a694d08af7577614bcee76f055e350b5d3615217
Emily Nelson <emilymay42@gmail.com>
2016-01-05 01:20:10 +0000

    Testing seed for db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+10.times do |x|
+  Quote.create(saying: "This is test saying #{x}", author: "Test Author #{x}")
+end
